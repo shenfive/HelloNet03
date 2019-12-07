56b867102bfef170737b95cb38ac41e80a874056
Danny Shen <shenfive@gmail.com>
2019-12-07 16:03:51 +0800

    sin

---
+++ HelloNet03/APIModel.swift
@@ -0,0 +1,20 @@
+//
+//  APIModel.swift
+//  HelloNet03
+//
+//  Created by 申潤五 on 2019/12/7.
+//  Copyright © 2019 申潤五. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+class APIModel {
+    static var share = APIModel()
+    private var apiURL = "https://randomuser.me/api/"
+    private init(){}
+
+}
+
+
