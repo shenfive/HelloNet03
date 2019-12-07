e0291519043914a52e2dfe94bef39e9ca41a0a62
Danny Shen <shenfive@gmail.com>
2019-12-07 16:58:04 +0800

    get name

--- HelloNet03/ViewController.swift
+++ HelloNet03/ViewController.swift
@@ -10,22 +10,28 @@ import UIKit
 import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         APIModel.share.queryRandomUserAlamofire { (data, error) in
-            if let theData = data as? Data{
+            if error != nil {
+                print(error?.localizedDescription)
+            }else{
                 do {
-                    let json = try JSON(data: theData)
-                    print(json)
-                } catch  {
+                    if let theData = data as? Data {
+                        let json = try JSON(data: theData)
+                        print(json)
+                        let lastname  = json["results"][0]["name"]["first"].stringValue
+                        print(lastname)
+                        
+                    }
+                } catch {
                     print(error.localizedDescription)
                 }
-                
             }
         }
     }
-
-
+    
+    
 }
 
